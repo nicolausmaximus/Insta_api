a332ad53c0a692c9847b67cbb95cb4ef69178bb4
nicolausmaximus <aniket261234@gmail.com>
2021-10-09 22:19:28 +0530

    added get posts

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Insta_api
+
+</br>
+To get started:</br>
+clone the repo</br>
+Change the current working directory to the location where you cloned the directory</br>
+Ensure you have golang installed(For this project, i have used 1.17)</br>
+Ensure mongodb is up and running(I have used the default port number 27017)</br>
+</br></br>
+To run the application, type </br>
+# go run app.go
+
+Now the server starts on port 3000</br>
+You can access it using Curl or postman
--- app.go
+++ app.go
@@ -1,9 +1,15 @@
-package appointy
+package main
 
 import (
 	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -11,10 +17,9 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type Person struct {
+type User struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name     string             `json:"name,omitempty" bson:"name,omitempty"`
 	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
@@ -22,41 +27,61 @@ type Person struct {
 }
 
 type Post struct {
-	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	User    primitive.ObjectID `json:"user,omitempty" bson:"user,omitempty"`
-	Caption string             `json:"caption,omitempty" bson:"caption,omitempty"`
-	Image   string             `json:"image,omitempty" bson:"image,omitempty"`
-	Time    string             `json:"time,omitempty" bson:"time,omitempty"`
+	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Caption    string             `json:"caption,omitempty" bson:"caption,omitempty"`
+	Image_URL  string             `json:"image_url,omitempty" bson:"image_url,omitempty"`
+	Time_stamp string             `json:"time_stamp,omitempty" bson:"time_stamp,omitempty"`
+	User       primitive.ObjectID `json:"user,omitempty" bson:"user,omitempty"`
 }
 
 var client *mongo.Client
-var postt []Post
+var postlist []Post
 
-//create Users Endpoint
-func CreateUsersEndpoint(response http.ResponseWriter, request *http.Request) {
-	response.Header().Add("content-type", "application/json")
-	var person Person
-	json.NewDecoder(request.Body).Decode(&person)
-	hash, err := bcrypt.GenerateFromPassword([]byte(person.Password), bcrypt.DefaultCost)
-	person.Password = string(hash)
+const BlockSize = 16
+
+//we are passing the passphrase to create a 32 byte key that will be used to hash the password
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+//hashing function -> used to hash the password
+func hash_password(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		fmt.Println(err)
+		panic(err.Error())
 	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func CreateNewUser(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var user User
+	json.NewDecoder(request.Body).Decode(&user)
+	hash := hash_password([]byte(user.Password), "ckdjjekk29i2")
+	user.Password = string(hash)
 	collection := client.Database("instagramAPI").Collection("users")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	result, _ := collection.InsertOne(ctx, person)
+	result, _ := collection.InsertOne(ctx, user)
 	json.NewEncoder(response).Encode(result)
 }
 
-func CreatePostsEndpoint(response http.ResponseWriter, request *http.Request) {
+func CreateNewPosts(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
 	var post Post
 	json.NewDecoder(request.Body).Decode(&post)
-	post.Time = time.Now().Format("2006-01-02 15:04:05")
+	post.Time_stamp = time.Now().Format("2001-10-26 20:59:20")
 	collection := client.Database("instagramAPI").Collection("posts")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	result, _ := collection.InsertOne(ctx, post)
-	postt = append(postt, post)
+	postlist = append(postlist, post)
 	json.NewEncoder(response).Encode(result)
 }
 
@@ -64,10 +89,10 @@ func GetUsersEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
 	params := mux.Vars(request)
 	id, _ := primitive.ObjectIDFromHex(params["id"])
-	var user Person
+	var user User
 	collection := client.Database("instagramAPI").Collection("users")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err := collection.FindOne(ctx, Person{ID: id}).Decode(&user)
+	err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
@@ -75,17 +100,32 @@ func GetUsersEndpoint(response http.ResponseWriter, request *http.Request) {
 	}
 	json.NewEncoder(response).Encode(user)
 }
+func GetPostsEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var post Post
+	collection := client.Database("instagramAPI").Collection("posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := collection.FindOne(ctx, Post{ID: id}).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+	json.NewEncoder(response).Encode(post)
+}
 
 func main() {
 	fmt.Println("Starting the application")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	router := mux.NewRouter()
-	router.HandleFunc("/users", CreateUsersEndpoint).Methods("POST")
+	router.HandleFunc("/users", CreateNewUser).Methods("POST")
 	router.HandleFunc("/users/{id}", GetUsersEndpoint).Methods("GET")
-	router.HandleFunc("/posts", CreatePostsEndpoint).Methods("POST")
+	router.HandleFunc("/posts", CreateNewPosts).Methods("POST")
 	router.HandleFunc("/posts/{id}", GetPostsEndpoint).Methods("GET")
 	router.HandleFunc("/posts/users/{id}", GetAllPostsEndpoint).Methods("GET")
 
-	http.ListenAndServe(":4000", router)
+	http.ListenAndServe(":1291", router)
 }
