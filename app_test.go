0a3fc4b9038a55138aa29a84e42e663b6fb82f8f
nicolausmaximus <aniket261234@gmail.com>
2021-10-09 23:01:38 +0530

    added unit testing3

--- app_test.go
+++ app_test.go
@@ -8,16 +8,16 @@ import (
 func TestHashing(t *testing.T) {
 	var bk []byte
 	got := hash_password([]byte("mynameisaniket"), "ckdjjekk29i2")
-	res :=bytes.Compare(got, bk)
-	if res!=0 {
+	res := bytes.Compare(got, bk)
+	if res != 0 {
 		t.Errorf("Correct")
 	}
 }
 func TestHashing_passphrase(t *testing.T) {
 	var bk string
 	ans := createHash("aniket")
-	res :=ans == bk
-	if res!=false {
+	res := ans == bk
+	if res != false {
 		t.Errorf("Correct")
 	}
 }
@@ -25,5 +25,6 @@ func TestHashing_passphrase(t *testing.T) {
 func BenchmarkAdd(b *testing.B) {
 	for i := 0; i < 5; i++ {
 		hash_password([]byte("mynameisaniket"), "ckdjjekk29i2")
+		createHash("aniket")
 	}
 }
